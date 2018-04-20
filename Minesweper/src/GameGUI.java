91a7ca206d8eb5cd7e025aba8dff6087e0be17e6
Raymond Li <raymond.jiayi@live.com>
2018-04-20 12:24:07 -0400

    Started GameGUI

--- Minesweper/src/GameGUI.java
+++ Minesweper/src/GameGUI.java
@@ -1,10 +1,20 @@
+
 /*
  * Authors: Raymond Li, David Tuck
  * Date started: 2018-04-18
  * Date Finished: 2018-04-
  * Description: Minesweeper game's main GUI
  */
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
 public class GameGUI {
+
+	// Private class Variables
+	JButton
+
 	public GameGUI(int mapSizeX, int mapSizeY) {
 
 	}
