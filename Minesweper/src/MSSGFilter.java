8568cfafb5affc7fca2286250885f56c71cee31e
David Tuck <32722002+tuck-david@users.noreply.github.com>
2018-04-20 11:27:46 -0400

    h

---
+++ Minesweper/src/MSSGFilter.java
@@ -0,0 +1,41 @@
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+public class MSSGFilter extends FileFilter {
+
+	public boolean accept(File f) {
+		if (f.isDirectory()) {
+			return true;
+		}
+
+		String extension = getExtension(f);
+		if (extension != null) {
+			if (extension.equals("mssg")) {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		return false;
+	}
+
+	// The description of this filter
+	public String getDescription() {
+		return "Minesweeper Savegames (.mssg files)";
+	}
+
+	// Gets extension of files
+	public static String getExtension(File f) {
+		String ext = null;
+		String s = f.getName();
+		int i = s.lastIndexOf('.');
+
+		if (i > 0 && i < s.length() - 1) {
+			ext = s.substring(i + 1).toLowerCase();
+		}
+		return ext;
+	}
+
+}
